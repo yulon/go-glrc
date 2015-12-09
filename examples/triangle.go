d000ac4017eb3a88643419e8971ecb7fba96503d
Yuchen Wang <yulon@me.com>
2015-12-09 21:01:11 +0100

    Update

--- examples/triangle.go
+++ examples/triangle.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"fmt"
-	"runtime"
-	"github.com/go-gl/gl/v2.1/gl"
 	"github.com/yulon/go-oswnd"
+	"github.com/yulon/go-gl"
 	"github.com/yulon/go-glrc"
 )
 
 func main() {
-	runtime.LockOSThread()
-
 	oswnd.Factory(func() {
 		wnd := oswnd.New()
 		wnd.SetTitle("Triangle")
@@ -20,12 +17,10 @@ func main() {
 		rc := glrc.New(wnd.GetId())
 		rc.Select()
 
-		if err := gl.Init(); err != nil {
-			panic(err)
-		}
-
 		version := gl.GoStr(gl.GetString(gl.VERSION))
 		fmt.Println("OpenGL version", version)
+
+		gl.ClearColor(1, 1, 1, 0)
 
 		wnd.OnPaint = func(){
 			gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
@@ -39,11 +34,16 @@ func main() {
 			gl.Vertex3f(-1.0, -1.0, 0.0)
 
 			gl.Color3f(0.0, 0.0, 1.0)
-			gl.Vertex3f(1.0, -1.0, 0.0)  
+			gl.Vertex3f(1.0, -1.0, 0.0)
 
 			gl.End()
 
 			rc.Render()
+		}
+
+		wnd.OnSize = func() {
+			cs := wnd.GetClientSzie()
+			gl.Viewport(0, 0, int32(cs.Width), int32(cs.Height))
 		}
 
 		wnd.SetLayout(oswnd.LayoutVisible)
